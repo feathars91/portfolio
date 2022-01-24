ad52ec78b0a46a8aec8fd2898b209dff7a2e557a
Feadars <abelino.chinchilla@gmail.com>
2022-01-24 13:31:29 -0600

    init

--- src/profile.js
+++ src/profile.js
@@ -9,42 +9,47 @@ const animation = {
 };
 // Change your display name on tha landing display
 const header = {
-  name: "a Human",
+  name: "Abel",
 };
 const background = {
   // Options: Snow or Particle
-  type: "Snow",
+  type: "Particle",
 };
 // Write a para about yourself here
 // To update your image, go to './styles/images.css'
 const section2title = "About Me";
 const about = {
   paragraph:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae suscipit tellus mauris a diam. ",
+    "My name is Abel and I am based in Houston, Texas, USA. I currently work for Disney as a developer.",
 };
 // Edit your skill and the percentage you know about it
 // To Add a skill, copy any one below and paste it after the last comma
 const skillsBar = [
   {
-    name: "HTML5",
+    name: "LAMBDA",
     // To add a custom svg instead of font-awesome icons, add svg path below otherwise just comment it out
-    svg: "M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z",
-    faClass: "fab fa-html5",
+    //svg: "M0 32l34.9 395.8L191.5 480l157.6-52.2L384 32H0zm308.2 127.9H124.4l4.1 49.4h175.6l-13.6 148.4-97.9 27v.3h-1.1l-98.7-27.3-6-75.8h47.7L138 320l53.5 14.5 53.7-14.5 6-62.2H84.3L71.5 112.2h241.1l-4.4 47.7z",
+    faClass: "fab fa-aws",
   },
   {
-    name: "CSS3",
+    name: "API GATEWAY",
     // svg: '',
-    faClass: "fab fa-css3",
+    faClass: "fab fa-aws",
   },
   {
-    name: "Javascript",
+    name: "COGNITO",
     // svg: '',
-    faClass: "fab fa-js",
+    faClass: "fab fa-aws",
   },
   {
-    name: "SASS",
+    name: "S3",
     // svg: '',
-    faClass: "fab fa-sass",
+    faClass: "fab fa-aws",
+  },
+  {
+    name: "DYNAMODB/REDSHIFT",
+    // svg: '',
+    faClass: "fas fa-database",
   },
   {
     name: "Node",
@@ -56,26 +61,6 @@ const skillsBar = [
     // svg: '',
     faClass: "fab fa-python",
   },
-  {
-    name: "Java",
-    // svg: '',
-    faClass: "fab fa-java",
-  },
-  {
-    name: "PHP",
-    // svg: '',
-    faClass: "fab fa-php",
-  },
-  {
-    name: "Database",
-    // svg: '',
-    faClass: "fas fa-database",
-  },
-  {
-    name: "AWS",
-    // svg: '',
-    faClass: "fab fa-aws",
-  },
 ];
 // Edit your projects, its name, your skills used to make it, and the url.
 // You can omit freely anything if you dont have it
@@ -83,46 +68,32 @@ const skillsBar = [
 const section3Title = "Past Projects";
 const projects = [
   {
-    // Add image in './styles/images.css' in #project1
-    id: "project1",
-    name: "Project 1",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
-  {
-    // Add image in './styles/images.css' in #project2
-    id: "project2",
-    name: "Project 2",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
-  },
-  {
     // Add image in './styles/images.css' in #project3
     id: "project3",
-    name: "Project 3",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    name: "AWS Data Pipeline - Step Functions",
+    skills: ["Lambda, Glue, Redshift, S3"],
+    url: "https://bit.ly/3fSQJXY",
   },
   {
     // Add image in './styles/images.css' in #project4
     id: "project4",
-    name: "Project 4",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    name: "Figma with DynamoDB",
+    skills: ["React, Figma, DynamoDB, Cognito"],
+    url: "https://master.d2be9othvc8guf.amplifyapp.com/",
   },
   {
-    // Add image in './styles/images.css' in #project5
-    id: "project5",
-    name: "Project 5",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    // Add image in './styles/images.css' in #project1
+    id: "project1",
+    name: "Vaccine Verification App",
+    skills: ["React, NodeJS, Lambda, API, Cognito, MYSQL"],
+    url: "https://master.dtkxb0wm4c9pi.amplifyapp.com/admin/index",
   },
   {
-    // Add image in './styles/images.css' in #project6
-    id: "project6",
-    name: "Project 6",
-    skills: ["HTML, CSS, JS"],
-    url: "https://github.com/kaustubhai",
+    // Add image in './styles/images.css' in #project2
+    id: "project2",
+    name: "Special Olympics Admin Site",
+    skills: ["AWS Amplify, NodeJS, Datatables"],
+    url: "https://www.soscadministration.org/admin/index",
   },
 ];
 // Edit your Miscellaneous Activities, its name and the url.
@@ -133,8 +104,9 @@ const miscellaneous = [
   {
     // Add image in './styles/images.css' in #misc1
     id: "misc1",
-    name: "Miscellaneous 1",
-    url: "https://github.com/kaustubhai",
+    name: "AWS Data Pipeline",
+    skills: ["Lambda, Glue, Redshift, S3"],
+    url: "https://bit.ly/3fSQJXY",
   },
   {
     // Add image in './styles/images.css' in #misc2
@@ -155,19 +127,20 @@ const miscellaneous = [
 const section5Title = "Get in Touch";
 const contact = {
   pitch:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
-  copyright: "Kaustubh Mishra",
+    "Feel free to message me for any work or consultation. I am at your order. Thank you.",
+  copyright: "",
   contactUrl: "",
 };
 // Paste your respective social media links. You can omit any if you dont have it
 // Upload your resume in your drive, get the shaareable link and paste it in the resume section
 const social = {
-  github: "https://github.com",
-  facebook: "https://facebook.com",
+
+  //facebook: "https://facebook.com",
   // twitter: "https://twitter.com",
-  instagram: "https://instagram.com",
-  linkedin: "https://linkedin.com",
-  resume: "https://novoresume.com/",
+  //instagram: "https://instagram.com",
+  linkedin: "www.linkedin.com/in/abelchinchilla",
+  resume: "https://portfolio-abel.s3.amazonaws.com/Abel's+Resume.pdf",
+    github: "https://github.com/feathars91",
 };
 // Dont change anything here
 export {
